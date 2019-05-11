35388597e340d8fd111d532ff0cdcc2e8fe6589c
nicolas <nico.damian.ex@gmail.com>
2019-05-11 06:01:53 +0200

    primer commit

---
+++ server/server.js
@@ -0,0 +1,39 @@
+require('./config/config')
+
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function(req, res) {
+    res.json('gET USUARIO')
+})
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'el nombre esncesario'
+        });
+    } else {
+        res.json({ persona: body })
+    }
+})
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json(id);
+})
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete USUARIO')
+})
+
+app.listen(process.env.PORT)
