819e80dc6f5748bf96e3629062b6719e999f5e4c
nicolas <nico.damian.ex@gmail.com>
2019-05-17 22:30:41 +0200

    subiendo

--- server/config/config.js
+++ server/config/config.js
@@ -8,10 +8,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //BASE DE DATOS
 let urlDB;
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
-} else {
-    urlDB = 'mongodb+srv://damian:cotolengo@cluster0-lghz8.mongodb.net/cafe?retryWrites=true';
-}
+//if (process.env.NODE_ENV === 'dev') {
+//   urlDB = 'mongodb://localhost:27017/cafe'
+//} else {
+urlDB = 'mongodb://uvv7s9po0zgauzld2w4i:APjyPBDxmeg3Rtrl5x9N@bgeevltcjiukgkt-mongodb.services.clever-cloud.com:27017/bgeevltcjiukgkt';
+//}
 
 process.env.URLDB = urlDB;
