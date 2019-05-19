12989cef4c26b32cdc5c3b97144dd143e799d0b6
nicolas <nico.damian.ex@gmail.com>
2019-05-19 06:55:51 +0200

    buena conexion

--- server/config/config.js
+++ server/config/config.js
@@ -4,14 +4,15 @@ process.env.PORT = process.env.PORT || 3000;
 
 //entorno
 
+//variable que establece heroku
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //BASE DE DATOS
 let urlDB;
-//if (process.env.NODE_ENV === 'dev') {
-//   urlDB = 'mongodb://localhost:27017/cafe'
-//} else {
-urlDB = 'mongodb://uvv7s9po0zgauzld2w4i:APjyPBDxmeg3Rtrl5x9N@bgeevltcjiukgkt-mongodb.services.clever-cloud.com:27017/bgeevltcjiukgkt';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb://uvv7s9po0zgauzld2w4i:APjyPBDxmeg3Rtrl5x9N@bgeevltcjiukgkt-mongodb.services.clever-cloud.com:27017/bgeevltcjiukgkt';
+}
 
 process.env.URLDB = urlDB;
